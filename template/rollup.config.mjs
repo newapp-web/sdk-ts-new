25a6126f5a7f68b3b2e39a28ec50e87c394534d1
newapp-web <wangshiwei@yahoo.com>
2023-02-23 15:38:35 +0800

    update

--- template/rollup.config.mjs
+++ template/rollup.config.mjs
@@ -4,15 +4,35 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import bundleSize from 'rollup-plugin-bundle-size';
 import { defineConfig } from 'rollup';
-
+const globalsConfig = {
+  eventemitter3: 'eventemitter3',
+  'crypto-js': 'CryptoJS',
+};
 const publicConfig = {
   format: 'umd',
-  name: '{{namespce}}',
-  globals: {
-    eventemitter3: 'eventemitter3',
-    'crypto-js': 'CryptoJS',
-  },
+  name: '{{namespace}}',
+  globals: globalsConfig,
 };
+
+const esConfig = {
+  format: 'esm',
+  globals: globalsConfig,
+};
+
+const pluginsConfig = [
+  babel({
+    babelHelpers: 'bundled',
+  }),
+  nodeResolve({
+    customResolveOptions: {
+      moduleDirectories: ['node_modules'],
+    },
+  }),
+  commonjs({
+    include: /node_modules/,
+  }),
+  bundleSize(),
+];
 
 const config = defineConfig([
   {
@@ -34,47 +54,28 @@ const config = defineConfig([
         ],
       },
     ],
-    plugins: [
-      babel({
-        babelHelpers: 'bundled',
-      }),
-      commonjs({
-        include: /node_modules/,
-      }),
-      nodeResolve({
-        customResolveOptions: {
-          moduleDirectories: ['node_modules'],
-        },
-      }),
-      bundleSize(),
-      // typescript(),
-    ],
+    plugins: pluginsConfig,
   },
   {
     input: 'src/index.js',
-    output: {
-      file: 'lib/index.es.js',
-      format: 'esm',
-      globals: {
-        eventemitter3: 'eventemitter3',
-        'crypto-js': 'CryptoJS',
+    output: [
+      {
+        file: 'lib/index.es.js',
+        ...esConfig,
       },
-    },
-    plugins: [
-      babel({
-        babelHelpers: 'bundled',
-      }),
-      nodeResolve({
-        customResolveOptions: {
-          moduleDirectories: ['node_modules'],
-        },
-      }),
-      commonjs({
-        include: /node_modules/,
-      }),
-      bundleSize(),
-      // typescript(),
+      {
+        file: 'lib/index.es.min.js',
+        ...esConfig,
+        plugins: [
+          terser({
+            compress: {
+              drop_console: true,
+            },
+          }),
+        ],
+      },
     ],
+    plugins: pluginsConfig,
   },
 ]);
 
